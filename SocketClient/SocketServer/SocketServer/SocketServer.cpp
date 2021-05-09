15e8b510d73e81002dae6afaeae9b6e41e69c370
pis3120 <40324511+pis3120@users.noreply.github.com>
2021-05-09 22:13:20 +0900

    Add files via upload

---
+++ SocketClient/SocketServer/SocketServer/SocketServer.cpp
@@ -0,0 +1,76 @@
+﻿#include "stdafx.h"
+#include "SocketServer.h"
+#include "SocketServerDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CSocketServerApp
+
+BEGIN_MESSAGE_MAP(CSocketServerApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CSocketServerApp 생성
+
+CSocketServerApp::CSocketServerApp()
+{
+	// 다시 시작 관리자 지원
+	m_dwRestartManagerSupportFlags = AFX_RESTART_MANAGER_SUPPORT_RESTART;
+
+}
+
+CSocketServerApp theApp;
+
+
+// CSocketServerApp 초기화
+
+BOOL CSocketServerApp::InitInstance()
+{
+
+	INITCOMMONCONTROLSEX InitCtrls;
+	InitCtrls.dwSize = sizeof(InitCtrls);
+	InitCtrls.dwICC = ICC_WIN95_CLASSES;
+	InitCommonControlsEx(&InitCtrls);
+
+	CWinApp::InitInstance();
+
+	if (!AfxSocketInit())
+	{
+		AfxMessageBox(IDP_SOCKETS_INIT_FAILED);
+		return FALSE;
+	}
+
+
+	AfxEnableControlContainer();
+
+\
+	CShellManager *pShellManager = new CShellManager;
+	CMFCVisualManager::SetDefaultManager(RUNTIME_CLASS(CMFCVisualManagerWindows));
+
+\
+	SetRegistryKey(_T("로컬 응용 프로그램 마법사에서 생성된 응용 프로그램"));
+
+	CSocketServerDlg dlg;
+	m_pMainWnd = &dlg;
+	INT_PTR nResponse = dlg.DoModal();
+
+
+	// 위에서 만든 셸 관리자를 삭제합니다.
+	if (pShellManager != nullptr)
+	{
+		delete pShellManager;
+	}
+
+#if !defined(_AFXDLL) && !defined(_AFX_NO_MFC_CONTROLS_IN_DIALOGS)
+	ControlBarCleanUp();
+#endif
+
+	// 대화 상자가 닫혔으므로 응용 프로그램의 메시지 펌프를 시작하지 않고  응용 프로그램을 끝낼 수 있도록 FALSE를
+	// 반환합니다.
+	return FALSE;
+}
+
